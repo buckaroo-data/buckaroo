fed9c51f0f6ae90ea9b467392e2887444e4476ad
paddymul <paddymul@users.noreply.github.com>
2026-02-18 19:58:11 +0000

    deploy: 33231ddc4a68a0f597ef883b9db4fdf897e68339

---
+++ src/stories/BuckarooWidgetTest.stories.d.ts
@@ -0,0 +1,12 @@
+import { StoryObj } from '@storybook/react';
+import { default as React } from '../../node_modules/.pnpm/react@18.3.1/node_modules/react';
+declare const meta: {
+    title: string;
+    component: React.FC<{}>;
+    parameters: {
+        layout: string;
+    };
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+export declare const Primary: Story;
