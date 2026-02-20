cef2ea53f4033a91f6f35b8b05c9c2bdd86b6001
paddymul <paddymul@users.noreply.github.com>
2026-02-20 23:05:51 +0000

    deploy: cc23f0b623107034c138c6f13add652fe62dba2e

--- src/components/useColorScheme.d.ts
+++
@@ -1,7 +0,0 @@
-type ColorScheme = 'light' | 'dark';
-/**
- * React hook that reactively tracks the user's OS color scheme preference.
- * Re-renders the component when the preference changes.
- */
-export declare function useColorScheme(): ColorScheme;
-export {};
