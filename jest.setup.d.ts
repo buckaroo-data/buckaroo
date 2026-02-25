1896ed6623dc6313b70b3883b2192e7e92d58b49
paddymul <paddymul@users.noreply.github.com>
2026-02-25 16:12:08 +0000

    deploy: 328a565911e6a8780d27dae75e07552ed924e8ad

---
+++ jest.setup.d.ts
@@ -0,0 +1,8 @@
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            toBeInTheDocument(): R;
+            toHaveClass(...classNames: string[]): R;
+        }
+    }
+}
