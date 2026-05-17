d13b8bde9b23c91961691b2dc42c5c1c779f4b25
paddymul <paddymul@users.noreply.github.com>
2026-05-17 16:31:01 +0000

    deploy: 49ab704f8f900fddb311cc09ee756b57c47e50ce

--- src/components/DFViewerParts/OtherRenderers.d.ts
+++ src/components/DFViewerParts/OtherRenderers.d.ts
@@ -1,5 +1,9 @@
 import { ValueFormatterFunc } from 'ag-grid-community';
 export declare const getTextCellRenderer: (formatter: ValueFormatterFunc<any>) => (props: any) => import("react/jsx-runtime").JSX.Element;
+export declare const getHighlightTextCellRenderer: (formatter: ValueFormatterFunc<any>, spec: {
+    phrase?: string | string[];
+    regex?: string;
+}, color?: string) => (props: any) => import("react/jsx-runtime").JSX.Element;
 export declare const LinkCellRenderer: (props: any) => import("react/jsx-runtime").JSX.Element;
 export declare const Base64PNGDisplayer: (props: any) => import("react/jsx-runtime").JSX.Element;
 export declare const SVGDisplayer: (props: any) => import("react/jsx-runtime").JSX.Element;
