1392fbf88039565a051c9f2cc36a92a2f75f0797
paddymul <paddymul@users.noreply.github.com>
2026-05-21 14:17:48 +0000

    deploy: fab83416d0e3b1ebd5f25d08a9a22ac6a87b4779

--- src/stories/StatusBar.stories.d.ts
+++ src/stories/StatusBar.stories.d.ts
@@ -2,10 +2,11 @@ import { StoryObj } from '@storybook/react';
 import { BuckarooOptions, BuckarooState, DFMeta } from '../components/WidgetTypes';
 declare const meta: {
     title: string;
-    component: ({ dfMeta, buckarooState, buckarooOptions, }: {
+    component: ({ dfMeta, buckarooState, buckarooOptions, inFlight, }: {
         dfMeta: DFMeta;
         buckarooState: BuckarooState;
         buckarooOptions: BuckarooOptions;
+        inFlight?: boolean;
     }) => import("react/jsx-runtime").JSX.Element;
     parameters: {
         layout: string;
@@ -17,3 +18,4 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 export declare const Primary: Story;
 export declare const NoCleaningNoPostProcessing: Story;
+export declare const InFlight: Story;
