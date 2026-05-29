6daf82f0f3135e792f4a701a14736616629998a9
paddymul <paddymul@users.noreply.github.com>
2026-05-29 15:09:07 +0000

    deploy: dc2c6a4cfe762b56fdb8d8a52da01f86a681fceb

---
+++ src/components/DFViewerParts/displayArgsUtils.d.ts
@@ -0,0 +1,2 @@
+import { IDisplayArgs } from './gridUtils';
+export declare function stampLayoutType(args: Record<string, IDisplayArgs>, autoHeight: boolean | undefined): Record<string, IDisplayArgs>;
