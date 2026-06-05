dd285095b69ae339029426d2e1bf5e86ebcb4efa
paddymul <paddymul@users.noreply.github.com>
2026-06-05 10:54:47 +0000

    deploy: ff17fc9a3300b6b92059097ec0b7883f4e8c34df

--- src/components/DFViewerParts/ChartCell.d.ts
+++ src/components/DFViewerParts/ChartCell.d.ts
@@ -1,9 +1,11 @@
+import { default as React } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
 import { ChartDisplayerA } from './DFWhole';
 export interface HistogramNode {
     name: string;
     population: number;
 }
 export declare const formatter: (value: any, name: any, props: any) => any[];
+export declare function FloatingTooltip({ items, x, y }: any): React.ReactPortal;
 export interface LineObservation {
     'unique'?: number;
     'barRed'?: number;
