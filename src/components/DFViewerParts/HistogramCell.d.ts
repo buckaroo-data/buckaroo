2c4583f5143b83c13fc7f46623fc794cc8e0ef43
paddymul <paddymul@users.noreply.github.com>
2026-06-05 10:56:06 +0000

    deploy: e8ce443879baf1fae5d391e5086a272b7b0e800b

--- src/components/DFViewerParts/HistogramCell.d.ts
+++ src/components/DFViewerParts/HistogramCell.d.ts
@@ -1,11 +1,9 @@
-import { default as React } from '../../../../node_modules/.pnpm/react@18.3.1/node_modules/react';
 import { ColDef, Column, Context, GridApi } from 'ag-grid-community';
 export interface HistogramNode {
     name: string;
     population: number;
 }
 export declare const formatter: (value: any, name: any, props: any) => any[];
-export declare function FloatingTooltip({ items, x, y }: any): React.ReactPortal;
 export interface HistogramBar {
     'cat_pop'?: number;
     'name': string;
