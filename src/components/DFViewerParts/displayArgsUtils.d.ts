f2f192915247735f340a415a6ae2cf8543df3ea8
paddymul <paddymul@users.noreply.github.com>
2026-06-06 10:49:01 +0000

    deploy: 47a6e239bbdee1e32e88d2215ee7f2906d74361b

--- src/components/DFViewerParts/displayArgsUtils.d.ts
+++ src/components/DFViewerParts/displayArgsUtils.d.ts
@@ -1,3 +1,2 @@
 import { IDisplayArgs } from './gridUtils';
-export declare function isFitContentLayout(args: Record<string, IDisplayArgs>): boolean;
 export declare function stampLayoutType(args: Record<string, IDisplayArgs>, autoHeight: boolean | undefined): Record<string, IDisplayArgs>;
